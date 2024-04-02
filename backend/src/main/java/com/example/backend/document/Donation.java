6e57fde0d0cab6e4c0e288bb58a8526487b0e6a8
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2024-04-02 15:54:48 -0500

    Added JSON Property

--- backend/src/main/java/com/example/backend/document/Donation.java
+++ backend/src/main/java/com/example/backend/document/Donation.java
@@ -3,11 +3,12 @@ package com.example.backend.document;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mongodb.lang.NonNull;
 import lombok.Builder;
 import lombok.Data;
 
-@Document(collection = "donations")
+@Document(collection = "donation")
 @Data
 @Builder
 public class Donation {
@@ -17,22 +18,27 @@ public class Donation {
 
     @NonNull
     @Field("donated_by")
+    @JsonProperty("donor_name")
     private String donorName;
 
     @NonNull
     @Field("donation_item")
+    @JsonProperty("donation_item")
     private String donationItem;
 
     @NonNull
     @Field("donation_category")
+    @JsonProperty("donation_category")
     private String donationCategory;
 
     @NonNull
     @Field("donation_datetime")
+    @JsonProperty("donation_datetime")
     private String donationDatetime;
 
     @NonNull
     @Field("is_claimed")
+    @JsonProperty("is_claimed")
     @Builder.Default
     private boolean isClaimed = false;
 }
